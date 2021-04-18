a273579b1ee11affa9ce6b572f2d69d7c219751d
Alex <42782806+alexsgi@users.noreply.github.com>
2021-04-18 15:34:40 +0200

    Update Application.java

--- changes_Alex/src/main/Application.java
+++ changes_Alex/src/main/Application.java
@@ -1,11 +1,11 @@
-package frontend;
+package main;
 
-import frontend.*;
+import frontend.FrameView;
+import frontend.StartView;
 
 public class Application {
 
 	public static void main(String[] args) {
-
 		StartView panel = new StartView();
 		FrameView view = new FrameView(panel);
 		view.setVisible(true);
