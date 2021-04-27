1405fb55eb0df366d48b8d687c4b9405ae0870a1
Alex <42782806+alexsgi@users.noreply.github.com>
2021-04-27 20:40:30 +0200

    UI Update

--- development/stickjumper/src/com/stickjumper/main/Main.java
+++ development/stickjumper/src/com/stickjumper/main/Main.java
@@ -1,8 +1,24 @@
 package com.stickjumper.main;
 
-public class Main {
-    public static void main(String[] args){
+import com.stickjumper.ui.frontend.FrameView;
+import com.stickjumper.ui.frontend.StartView;
 
-        System.out.println("lol");
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        initUI();
+        StartView panel = new StartView();
+        FrameView view = new FrameView(panel);
+        view.setVisible(true);
+    }
+
+    private static void initUI() {
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
 }
--- development/stickjumper/src/com/stickjumper/ui/frontend/TEST.java
+++
@@ -1,4 +0,0 @@
-package com.stickjumper.ui.frontend;
-
-public class TEST {
-}
