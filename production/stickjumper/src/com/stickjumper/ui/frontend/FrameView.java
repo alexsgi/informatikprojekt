5e2e594470526e203998d8e7a0c183a58fb11271
Alex <42782806+alexsgi@users.noreply.github.com>
2021-04-28 17:56:11 +0200

    PRODUCTION UPDATE

---
+++ production/stickjumper/src/com/stickjumper/ui/frontend/FrameView.java
@@ -0,0 +1,17 @@
+package com.stickjumper.ui.frontend;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class FrameView extends JFrame {
+
+    public FrameView(JPanel contentPane) {
+        setResizable(false);
+        setTitle("StickJumper");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1280, 640);
+        setLocationRelativeTo(null);
+        setContentPane(contentPane);
+    }
+
+}
---
+++ production/stickjumper/src/com/stickjumper/ui/frontend/StartView.java
@@ -0,0 +1,62 @@
+package com.stickjumper.ui.frontend;
+
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+public class StartView extends JPanel {
+
+    private Font AHARONI_FONT;
+
+    public StartView() {
+        registerFont();
+        setLayout(null);
+        JLabel lblTitel = new JLabel("StickJumper");
+        lblTitel.setHorizontalAlignment(SwingConstants.CENTER);
+        lblTitel.setBounds(0, 96, 1280, 83);
+        lblTitel.setFont(AHARONI_FONT);
+        //lblTitel.setFont(lblTitel.getFont().deriveFont(64f));
+        // lblTitel.setFont(AHARONI_FONT, Font.PLAIN, 30);
+        add(lblTitel);
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics graphicsObject) {
+        super.paintComponent(graphicsObject);
+        Image bgImage;
+        try {
+            bgImage = ImageIO.read(getClass().getResource("/res/images/mountains-middle.png"));
+            graphicsObject.drawImage(bgImage, 0, 0, null);
+        } catch (IOException exceptionObject) {
+            exceptionObject.printStackTrace();
+        }
+    }
+
+    private void registerFont() {
+        try {
+            Font AHARONI_FONT = Font.createFont(Font.TRUETYPE_FONT,
+                    new File(getClass().getResource("/res/fonts/aharoni.ttf").toURI()));
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            boolean registerSuccess = ge.registerFont(AHARONI_FONT);
+            if(registerSuccess) {
+                System.out.println("Schriftart registriert!");
+            } else {
+                System.err.println("Schriftart konnte nicht registriert werden!");
+            }
+        } catch (IOException | URISyntaxException | FontFormatException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
