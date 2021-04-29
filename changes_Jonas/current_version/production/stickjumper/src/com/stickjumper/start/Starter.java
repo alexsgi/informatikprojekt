0faa03f69c75e0c98a43f8310b63d9e10a164747
Jonas-str <68121374+Jonas-str@users.noreply.github.com>
2021-04-29 09:54:49 +0200

    tried relative path

--- changes_Jonas/current_version/production/stickjumper/src/com/stickjumper/start/Starter.java
+++ changes_Jonas/current_version/production/stickjumper/src/com/stickjumper/start/Starter.java
@@ -5,6 +5,9 @@ import com.stickjumper.ui.frontend.StartView;
 
 import javax.swing.UIManager;
 import java.awt.*;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 
 public class Starter {
@@ -16,7 +19,8 @@ public class Starter {
         view.setVisible(true);
 
         // Create an image instance from the image that you want to use as icon for your app
-        Image icon = Toolkit.getDefaultToolkit().getImage("C:\\Users\\jonas\\Documents\\Projekte\\informatikprojekt\\changes_Jonas\\current_version\\production\\stickjumper\\src\\res\\images\\ic_launcher.png");
+        String path="C:\\Users\\jonas\\Documents\\Projekte\\informatikprojekt\\changes_Jonas\\current_version\\production\\stickjumper\\src\\com\\stickjumper\\start";
+        Image icon = Toolkit.getDefaultToolkit().getImage(path+"\\appicon_5.png");
         // And set it
         view.setIconImage(icon);
     }
