86f5959444d12bd947fc0ed1139477bf932e8f4e
Jonas-str <68121374+Jonas-str@users.noreply.github.com>
2021-04-29 12:29:59 +0200

    textfield at login

--- changes_Jonas/current_version/production/stickjumper/src/com/stickjumper/ui/frontend/LoginPanelView.java
+++ changes_Jonas/current_version/production/stickjumper/src/com/stickjumper/ui/frontend/LoginPanelView.java
@@ -9,8 +9,12 @@ import java.io.IOException;
 
 public class LoginPanelView  extends JPanel {
 
+    private TextField welcomeTextField = new TextField();
     public LoginPanelView() {
-
+        welcomeTextField.setText("Welcome to our service");
+        add(welcomeTextField);
+        welcomeTextField.setBounds(20, 20, 200, 50);
+        welcomeTextField.setFont(new Font("Arial Black", Font.PLAIN, 30));
     }
 
 }
