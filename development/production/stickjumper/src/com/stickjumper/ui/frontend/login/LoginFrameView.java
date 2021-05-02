af3b8bb8cd972fc6441abe3e096818617eb2da59
Jonas-str <68121374+Jonas-str@users.noreply.github.com>
2021-05-02 18:32:39 +0200

    round corners at the login view

--- development/production/stickjumper/src/com/stickjumper/ui/frontend/login/LoginFrameView.java
+++ development/production/stickjumper/src/com/stickjumper/ui/frontend/login/LoginFrameView.java
@@ -1,6 +1,7 @@
 package com.stickjumper.ui.frontend.login;
 
 import javax.swing.*;
+import java.awt.geom.RoundRectangle2D;
 
 public class LoginFrameView extends JFrame {
 
@@ -8,6 +9,9 @@ public class LoginFrameView extends JFrame {
         setResizable(false);
         setTitle("Login");
         setSize(600, 500);
+        setUndecorated(true);
+        int arc = 18;
+        setShape(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), arc, arc));
         setLocationRelativeTo(null);
         setContentPane(contentPane);
     }
