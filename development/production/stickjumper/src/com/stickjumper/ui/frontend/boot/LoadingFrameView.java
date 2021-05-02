5add8a14259bbe18170be87881dea2f5681d228e
Alex <42782806+alexsgi@users.noreply.github.com>
2021-05-02 17:45:21 +0200

    Update

--- development/production/stickjumper/src/com/stickjumper/ui/frontend/boot/LoadingFrameView.java
+++ development/production/stickjumper/src/com/stickjumper/ui/frontend/boot/LoadingFrameView.java
@@ -3,6 +3,7 @@ package com.stickjumper.ui.frontend.boot;
 import com.stickjumper.start.Starter;
 
 import javax.swing.*;
+import java.awt.geom.RoundRectangle2D;
 
 public class LoadingFrameView extends JFrame {
 
@@ -12,7 +13,7 @@ public class LoadingFrameView extends JFrame {
         setSize(1080, 300);
         setLocationRelativeTo(null);
         setContentPane(contentPane);
-
+        setShape(new RoundRectangle2D.Double(0, 0, 1080, 300, 50, 50));
         setIconImage(Starter.getImage(getClass(), "/images/icons/appicon_5.png"));
     }
 
