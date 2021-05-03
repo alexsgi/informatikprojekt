85e0fbaa60b1c2bb84dcb9167b1175f7429087fe
Alex <42782806+alexsgi@users.noreply.github.com>
2021-05-03 08:20:11 +0200

    Create Controller.java

---
+++ development/production/stickjumper/src/com/stickjumper/ui/frontend/Controller.java
@@ -0,0 +1,10 @@
+package com.stickjumper.ui.frontend;
+
+import com.stickjumper.ui.frontend.boot.LoadingFrameView;
+
+public class Controller {
+
+    private MainFrameView mainFrameView;
+    private LoadingFrameView loadingFrameView;
+
+}
