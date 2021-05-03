c70ceb1aeecf4cbb5e500ec6f45d4b9e1bd18bb5
Alex <42782806+alexsgi@users.noreply.github.com>
2021-05-03 08:26:22 +0200

    Update GamePanelView.java

--- development/production/stickjumper/src/com/stickjumper/ui/frontend/GamePanelView.java
+++ development/production/stickjumper/src/com/stickjumper/ui/frontend/GamePanelView.java
@@ -7,13 +7,12 @@ import java.awt.*;
 
 public class GamePanelView extends JPanel {
 
-
     private final Font MAIN_FONT = UITools.registerFont();
     private JLabel lblTitle;
 
     public GamePanelView() {
         setLayout(null);
-        lblTitle = new JLabel("StickJumper");
+        lblTitle = new JLabel("GamePanel");
         lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
         lblTitle.setBounds(0, 96, 1280, 83);
         lblTitle.setFont(MAIN_FONT);
