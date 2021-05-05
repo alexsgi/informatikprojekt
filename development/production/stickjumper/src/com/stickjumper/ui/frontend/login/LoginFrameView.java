d09241815179cb728f2e5c45e2c7576503f3aa11
Alex <42782806+alexsgi@users.noreply.github.com>
2021-05-05 11:20:47 +0200

    Update LoginFrameView.java

--- development/production/stickjumper/src/com/stickjumper/ui/frontend/login/LoginFrameView.java
+++ development/production/stickjumper/src/com/stickjumper/ui/frontend/login/LoginFrameView.java
@@ -15,9 +15,6 @@ public class LoginFrameView extends JFrame {
         setResizable(false);
         setTitle("Login");
         setSize(600, 500);
-        //setUndecorated(true);
-        //int arc = 18;
-        //setShape(new RoundRectangle2D.Double(0, 0, getWidth(), getHeight(), arc, arc));
         setLocationRelativeTo(null);
         setContentPane(contentPane);
 
