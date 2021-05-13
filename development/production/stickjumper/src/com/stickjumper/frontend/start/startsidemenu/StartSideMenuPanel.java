2d24acaee5211d6c7954ced295583aa8d9173ef5
Jonas-str <68121374+Jonas-str@users.noreply.github.com>
2021-05-13 20:47:40 +0200

    reanme StartSideMenuPanel

--- development/production/stickjumper/src/com/stickjumper/frontend/start/startsidemenu/StartSideMenuPanel.java
+++ development/production/stickjumper/src/com/stickjumper/frontend/start/startsidemenu/StartSideMenuPanel.java
@@ -1,10 +1,7 @@
 package com.stickjumper.frontend.start.startsidemenu;
 
-import com.stickjumper.utils.UITools;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 public class StartSideMenuPanel extends JPanel {
 
@@ -13,14 +10,6 @@ public class StartSideMenuPanel extends JPanel {
         setSize(parent.getWidth() / 7, parent.getHeight());
         setLocation(0, 0);
         setBackground(new Color(86, 73, 78, 125));
-    }
-
-    @Override
-    protected void paintComponent(Graphics graphicsObject) {
-        super.paintComponent(graphicsObject);
-        BufferedImage image = UITools.getImage(getClass(), "/images/start_view/background/mountains-middle.png");
-        if (image == null) return;
-        graphicsObject.drawImage(image, 0, 0, null);
     }
 
 }
