70870b26d4827827f59496181378b2fedad5d223
Alex <sagorski.entwicklung@gmail.com>
2021-05-19 09:30:56 +0200

    PWHasher

--- development/production/stickjumper/src/com/stickjumper/utils/security/PasswordHasher.java
+++ development/production/stickjumper/src/com/stickjumper/utils/security/PasswordHasher.java
@@ -11,26 +11,20 @@ import java.util.Base64;
 
 public class PasswordHasher {
 
-    private static byte[] salt = new byte[]{-71, 0, -54, 50, 22, -16, -67, -84, 36, 17, -31, -104, -23, -112, -77, 123};
+    private static final byte[] salt = new byte[]{-71, 0, -54, 50, 22, -16, -67, -84, 36, 17, -31, -104, -23, -112, -77, 123};
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException {
-        long start, end;
-        String passwordToHash = "das123Ist!EinÜasswort", hashed;
-        for (int i = 0; i <= 2; i++) {
-            start = System.currentTimeMillis();
-            hashed = hash(passwordToHash);
-            end = System.currentTimeMillis();
-            Settings.logData("Hashed password: " + hashed);
-            Settings.logData("Hashing took " + (end - start) + " ms\n");
-        }
-    }
-
-    public static String hash(String input) throws NoSuchAlgorithmException, InvalidKeySpecException {
+    public static String hash(String input) {
         KeySpec spec = new PBEKeySpec(input.toCharArray(), salt, 65536, 128);
-        SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
-        byte[] hash = f.generateSecret(spec).getEncoded();
-        Base64.Encoder enc = Base64.getEncoder();
-        return enc.encodeToString(hash);
+        try {
+            SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            byte[] hash = f.generateSecret(spec).getEncoded();
+            Base64.Encoder enc = Base64.getEncoder();
+            return enc.encodeToString(hash);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+            Settings.logData("Error hashing password", e);
+        }
+        return null;
     }
 
 }
