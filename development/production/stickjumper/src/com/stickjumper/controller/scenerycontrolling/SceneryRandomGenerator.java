3d28d3f5fd1a9d922eadb00ac2f6acd01f7dba8f
Jonas-str <68121374+Jonas-str@users.noreply.github.com>
2021-05-22 21:48:05 +0200

    SceneryRandomGenerator

--- development/production/stickjumper/src/com/stickjumper/controller/scenerycontrolling/SceneryRandomGenerator.java
+++ development/production/stickjumper/src/com/stickjumper/controller/scenerycontrolling/SceneryRandomGenerator.java
@@ -4,15 +4,28 @@ public class SceneryRandomGenerator {
 
     private SceneryController sceneryController;
 
+    // array lists for all objects
+
 
     public SceneryRandomGenerator(){
-        // missing random component
+        generate();
         // TODO: creating all the objects "enemy, steadyObstacle, Coin" in here and passing them as input parameter in the method "initCertainObject"
         // therefore, you have to adapt the method in SceneryController for passing an object of the class GameElement as an input parameter
     }
 
     public void setSceneryController(SceneryController sceneryController) {
         this.sceneryController = sceneryController;
+    }
+
+    public void generate(){
+        // the real random component
+    }
+
+    public void recreate(){
+        generate();
+        // dont know if this is really needed
+
+        // overwrites all the objects in the array lists, so that the game is different form the last one
     }
 
 }
