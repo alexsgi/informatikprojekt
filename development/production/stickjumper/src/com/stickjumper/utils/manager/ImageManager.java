5ae22de6cfe3b7ab2ff41bf28261eb9ade586109
Alex <sagorski.entwicklung@gmail.com>
2021-06-27 12:09:35 +0200

    Coin reverse

--- development/production/stickjumper/src/com/stickjumper/utils/manager/ImageManager.java
+++ development/production/stickjumper/src/com/stickjumper/utils/manager/ImageManager.java
@@ -34,11 +34,11 @@ public class ImageManager {
         STEADY_OBSTACLE_SKIN = UITools.getImage(c, "/images/elements/obstacles/steady_obstacle_skin.png");
         ICON_INTERNET_AVAILABLE = UITools.getImage(c, "/images/start_view/icons/internet_available.png");
         ICON_INTERNET_UNAVAILABLE = UITools.getImage(c, "/images/start_view/icons/internet_unavailable.png");
-        COIN_SKIN_10 = UITools.getImage(c, "/images/elements/coin/coin_1.png");
-        COIN_SKIN_20 = UITools.getImage(c, "/images/elements/coin/coin_2.png");
-        COIN_SKIN_30 = UITools.getImage(c, "/images/elements/coin/coin_3.png");
-        COIN_SKIN_40 = UITools.getImage(c, "/images/elements/coin/coin_4.png");
-        COIN_SKIN_50 = UITools.getImage(c, "/images/elements/coin/coin_5.png");
+        COIN_SKIN_10 = UITools.getImage(c, "/images/elements/coin/coin_10.png");
+        COIN_SKIN_20 = UITools.getImage(c, "/images/elements/coin/coin_20.png");
+        COIN_SKIN_30 = UITools.getImage(c, "/images/elements/coin/coin_30.png");
+        COIN_SKIN_40 = UITools.getImage(c, "/images/elements/coin/coin_40.png");
+        COIN_SKIN_50 = UITools.getImage(c, "/images/elements/coin/coin_50.png");
     }
 
     public static int getSizeTolerance() {
